a12f2cc6c8c4520755eded830753d64f321a7e3c
Lauri Ellen <ich.lauri@gmail.com>
2024-06-17 15:20:58 -0300

    Create poo.model Classes

---
+++ Projects/prjPOO3/src/fatec/poo/model/Vendedor.java
@@ -0,0 +1,49 @@
+package fatec.poo.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ichla
+ */
+public class Vendedor extends Pessoa{
+    private double salarioBase;
+    private double taxaComissao;
+    private ArrayList<Pedido> pedidosVendedor;
+    
+    public Vendedor(String nome, String cpf, double salarioBase) {
+        super(nome, cpf);
+        this.salarioBase = salarioBase;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double getTaxaComissao() {
+        return taxaComissao;
+    }
+
+    public void setTaxaComissao(double taxaComissao) {
+        this.taxaComissao = taxaComissao / 100;
+    }
+    
+    public void addPedidoVendedor(Pedido pedidosVendedor) {
+        this.pedidosVendedor.add(pedidosVendedor);
+        pedidosVendedor.setVendedor(this);
+    } 
+     
+    public void listarPedidos(){
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+    }
+}
